190afd7ae8b5511ece45e2d57ccaeeacd009bca5
Aaron Juarez <aarjuamtz@gmail.com>
2016-06-06 11:40:04 -0500

    Fix: Analytics file added, font request deleted

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+(function(w,g){
+	w['GoogleAnalyticsObject']=g;
+    w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};
+    w[g].l=1*new Date();
+})(window,'ga');
+ga('create', 'UA-78845320-1');
+ga('send', 'pageview');
